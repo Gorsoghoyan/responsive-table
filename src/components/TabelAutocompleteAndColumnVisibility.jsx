0b8954e8371044c0cbe97218c2fb4f68db927d10
Gorsoghoyan <soghoyangor99@gmail.com>
2023-05-15 18:44:25 +0400

    push

--- src/components/TabelAutocompleteAndColumnVisibility.jsx
+++ src/components/TabelAutocompleteAndColumnVisibility.jsx
@@ -1,10 +1,9 @@
 import s from "../sass/table.module.scss";
 
 export default function TabelAutocompleteAndColumnVisibility() {
-
   return (
     <div className={s.top}>
-      top
+      
     </div> 
   );
 }
