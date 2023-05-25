1191a9a0294d0d3ede5e3164c8ba7507b4abad2a
Gorsoghoyan <soghoyangor99@gmail.com>
2023-05-26 01:33:40 +0300

    push

--- src/pages/Table/Table.jsx
+++ src/pages/Table/Table.jsx
@@ -2,13 +2,21 @@ import TabelSearchAndColumnVisibility from "./TabelSearchAndColumnVisibility";
 import TablePagination from "./TablePagination";
 import s from "./styles.module.scss";
 
+const TableRow = ({ children }) => <tr>{children}</tr>;
+
+const TableHeadCell = ({ children }) => <th>{children}</th>;
+
+const TableBodyCell = ({ children }) => <td>{children}</td>;
+
 export default function Table() {
   return (
     <div className={s.tableContainer}>
       <TabelSearchAndColumnVisibility />
       <table className={s.table}>
         <thead>
-
+          <TableRow>
+            {}
+          </TableRow>
         </thead>
         <tbody>
 
---
+++ src/pages/Table/useTable.js
@@ -0,0 +1,8 @@
+
+export const useTable = () => {
+  
+
+  return {
+
+  };
+};
